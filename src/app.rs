d874f1244fcbe8b49c7603877184c781b83cab84
Matthew Klein <mklein994@gmail.com>
2018-02-04 18:19:17 -0600

    Calculate duration between two dates

--- src/app.rs
+++ src/app.rs
@@ -8,7 +8,25 @@ pub fn build_cli<'a>() -> ArgMatches<'a> {
             Arg::with_name("duration")
                 .short("d")
                 .long("duration")
-                .takes_value(true),
+                .takes_value(true)
+                .conflicts_with("difference"),
+        )
+        .arg(
+            Arg::with_name("begin")
+                .short("b")
+                .long("begin")
+                .aliases(&["start", "s"])
+                .takes_value(true)
+                .requires("end")
+                .group("difference"),
+        )
+        .arg(
+            Arg::with_name("end")
+                .short("e")
+                .long("end")
+                .aliases(&["finish", "f"])
+                .takes_value(true)
+                .group("difference"),
         )
         .get_matches()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate clap;
 pub mod app;
 mod error;
 
+use chrono::{DateTime, Local, TimeZone};
 use clap::ArgMatches;
 use self::error::Error;
 
@@ -12,5 +13,18 @@ type Result<T> = std::result::Result<T, Error>;
 
 pub fn run(matches: ArgMatches) -> Result<()> {
     eprintln!("{:#?}", matches);
+
+    if matches.is_present("duration") {
+        let seconds = matches.value_of("duration").unwrap().parse::<usize>()?;
+
+        print_duration(seconds)?;
+    } else if matches.is_present("difference") {
+        unimplemented!();
+    }
+
     Ok(())
+}
+
+fn print_duration(s: usize) -> Result<()> {
+    unimplemented!()
 }
