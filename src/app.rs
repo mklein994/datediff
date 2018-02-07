19502b489acd79acc0437935d259c7e2c1171f89
Matthew Klein <mklein994@gmail.com>
2018-02-06 20:45:16 -0600

    Logically sort clap argument options and relationships

--- src/app.rs
+++ src/app.rs
@@ -11,31 +11,33 @@ pub fn build_cli() -> App<'static, 'static> {
         )
         .arg(
             Arg::with_name("difference")
+                .help("Calculate the difference between two dates. Assume today if given only one.")
                 .short("d")
                 .long("difference")
-                .help(
-                    "Calculate the difference between two dates. Assuming today if given only one.",
-                )
                 .alias("diff")
-                .value_name("date")
-                .max_values(2),
+                .max_values(2)
+                .value_name("date"),
         )
         .arg(
             Arg::with_name("seconds")
+                .help("Parse arguments as UNIX timestamps.")
                 .short("s")
                 .long("seconds")
-                .help("Parse arguments as UNIX timestamps.")
                 .conflicts_with("duration"),
         )
         .arg(
             Arg::with_name("rough")
+                .help("Use rough accuracy. The default is to be precise.")
                 .short("r")
-                .long("rough")
-                .help("Use rough accuracy. The default is to be precise."),
+                .long("rough"),
         )
         .subcommand(
             SubCommand::with_name("completions")
-                .help("write a tab completion script to standard output for the provided shell.")
-                .arg(Arg::with_name("shell").possible_values(&Shell::variants())),
+                .help("Write a tab completion script to standard output for the provided shell.")
+                .arg(
+                    Arg::with_name("shell")
+                        .help("The shell to provide completion for.")
+                        .possible_values(&Shell::variants()),
+                ),
         )
 }
